07852af374ed48b16c88a7620ca69ce1cbe2475e
Matthew Lorber <matthew.lorber@gmail.com>
2023-05-01 12:30:12 -0500

    tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        artist VARCHAR(255),
+                        name VARCHAR(255),
+                        release_date INT,
+                        sales FLOAT,
+                        genre VARCHAR(255)
+);
